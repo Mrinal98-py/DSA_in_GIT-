8fb49a834a34b1adf202f619f6a5084a1c660a59
Mrinal Raj <67463977+Mrinal98-py@users.noreply.github.com>
2025-02-24 17:16:09 +0530

    24-2-25

---
+++ linkedlistpractive.py
@@ -0,0 +1,17 @@
+class hownodewilllooklike:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+    
+class linkedlist:
+    def __init__(self):
+        self.head = None   #head will be empty in starting 
+        
+        def insert(self, data):
+            new_node = hownodewilllooklike(data)
+            
+            if not self.head:
+                self.head = new_node
+        
+        
+    
